f7ee6ce896565be8f204621aa851129f0e337882
Emman-pip <115330041+Emman-pip@users.noreply.github.com>
2023-09-15 17:45:02 +0800

    Add observer code and map on section 1.

--- index.js
+++ index.js
@@ -3,6 +3,34 @@ const changeFontfam = (className, classToAdd) => {
     element.classList.add(classToAdd);
   });
 };
+
+function animationTrigger(section, animationName, name, expand = null) {
+  const options = {
+    root: null,
+    threshold: 0,
+    rootMargin: "25px",
+  };
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.target.className.includes(name) && entry.isIntersecting) {
+        entry.target.classList.add(expand);
+      } else if (
+        entry.target.className.includes(name) &&
+        !entry.isIntersecting
+      ) {
+        entry.target.classList.remove(expand);
+      }
+      if (entry.isIntersecting) {
+        entry.target.classList.add(animationName);
+      } else {
+        entry.target.classList.remove(animationName);
+      }
+    });
+  }, options);
+
+  observer.observe(section);
+}
+
 (() => {
   changeFontfam(document.querySelectorAll(".nav-link"), "fs-5");
   changeFontfam(document.querySelectorAll(".checks > div"), "fs-5");
